c7a213e1f217216a7e2e0c6c2ebff0920788c13f
Santhoshreddy746 <40651570+Santhoshreddy746@users.noreply.github.com>
2019-01-11 14:22:11 -0600

    Update install_mediawiki.sh

--- install_mediawiki.sh
+++ install_mediawiki.sh
@@ -1,1 +1,12 @@
-echo "Hello"
+sudo apt-get update && sudo apt-get upgrade
+
+sudo apt-get install apache2 mysql-server php php-mysql libapache2-mod-php php-xml php-mbstring
+
+cd /tmp/wget https://releases.wikimedia.org/mediawiki/1.31/mediawiki-1.31.1.tar.gz
+
+tar -xvzf /tmp/mediawiki-*.tar.gz
+sudo mkdir /var/lib/mediawiki
+sudo mv mediawiki-*/* /var/lib/mediawiki
+
+
+sudo ln -s /var/lib/mediawiki /var/www/html/mediawiki
