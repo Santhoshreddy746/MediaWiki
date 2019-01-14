a7bb963d4303c4475f4cd5e6bea960a41284e40b
Santhoshreddy746 <40651570+Santhoshreddy746@users.noreply.github.com>
2019-01-14 13:54:47 -0600

    Update install_mediawiki.sh

--- install_mediawiki.sh
+++ install_mediawiki.sh
@@ -2,7 +2,7 @@ sudo apt -y update
 
 
 #echo Y | sudo apt update && sudo apt install lamp-server^
-sudo apt -y install apache2 mysql-server php php-mysql libapache2-mod-php php-xml php-mbstring
+sudo apt-get -y install apache2 mysql-server php php-mysql libapache2-mod-php php-xml php-mbstring
 
 cd /tmp/wget https://releases.wikimedia.org/mediawiki/1.31/mediawiki-1.31.1.tar.gz
 
