8b0b7473015c4eed08f5a590015863ee65f544fe
Santhoshreddy746 <40651570+Santhoshreddy746@users.noreply.github.com>
2019-01-15 10:08:57 -0600

    Update install_mediawiki.sh

--- install_mediawiki.sh
+++ install_mediawiki.sh
@@ -1,20 +1,46 @@
+#!/bin/bash
+cd /tmp
+# Install LAMP
+sudo apt-get update -y
+sudo apt-get install -y apache2 mysql-server php php-mysql libapache2-mod-php php-xml php-mbstring
 
-sudo rm /var/lib/dpkg/lock-frontend
+# mysql_secure_installation and create mediawiki user
+export MYSQL_ROOT_PASSWORD="media"
+mysql -uroot << 'EOF'
+UPDATE mysql.user SET authentication_string=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';
+DELETE FROM mysql.user WHERE user='root' AND host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.user WHERE user='';
+CREATE DATABASE mediawiki;
+CREATE USER 'media'@'localhost' IDENTIFIED BY 'media';
+GRANT ALL ON mediawiki.* TO 'media'@'localhost' IDENTIFIED BY 'media' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
 
-sudo rm /var/lib/dpkg/lock
+# Mediawiki installation
+wget https://releases.wikimedia.org/mediawiki/1.31/mediawiki-1.31.0.tar.gz
+tar -xvzf mediawiki-1.31.0.tar.gz
+sudo cp -r mediawiki-1.31.0 /var/www/html/mediawiki
+sudo chown -R www-data:www-data /var/www/html/mediawiki
+sudo chmod -R 777 /var/www/html/mediawiki
 
-sudo dpkg --configure -a
-sudo apt -y update
+# Create Apache virtual host for mediawiki
+touch /etc/apache2/sites-available/mediawiki.conf
+cat <<EOT >> /etc/apache2/sites-available/mediawiki.conf
+<VirtualHost *:80>
+ServerAdmin admin@example.com
+DocumentRoot /var/www/html/mediawiki/
+ServerName example.com
+<Directory /var/www/html/mediawiki/>
+Options +FollowSymLinks
+AllowOverride All
+</Directory>
+ErrorLog /var/log/apache2/media-error_log
+CustomLog /var/log/apache2/media-access_log common
+</VirtualHost>
+EOT
+mv /var/www/html/index.html /var/www/html/index.html_back
+sudo a2ensite mediawiki.conf
+sudo a2enmod rewrite
 
-
-#echo Y | sudo apt update && sudo apt install lamp-server^
-sudo apt-get -y install apache2 mysql-server php php-mysql libapache2-mod-php php-xml php-mbstring
-
-cd /tmp/wget https://releases.wikimedia.org/mediawiki/1.31/mediawiki-1.31.1.tar.gz
-
-tar -xvzf /tmp/mediawiki-*.tar.gz
-sudo mkdir /var/lib/mediawiki
-sudo mv mediawiki-*/* /var/lib/mediawiki
-
-
-yes | sudo ln -s /var/lib/mediawiki /var/www/html/mediawiki
+# Restart Apache
+sudo systemctl restart apache2
